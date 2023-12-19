adc4db7c2c362b67f54254b522209c75ca9dd005
Zaid Irfan <147980922+zirfan1@users.noreply.github.com>
2023-12-19 20:41:39 +0000

    Update hangman.py
    Added a Two-Player feature to this game

--- hangman.py
+++ hangman.py
@@ -1,95 +1,89 @@
-import random
-
-def choose_word():
-    words = ["python", "hangman", "programming", "challenge"]
-    return random.choice(words)
+import getpass
 
 def display_word(word, guessed_letters):
     return " ".join([letter if letter in guessed_letters else "_" for letter in word])
 
-def display_hangman(attempts):
-    stages = [  # Final state: head, torso, both arms, and both legs
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / \\
-                   -
-                """,
-                # Head, torso, both arms, and one leg
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |     / 
-                   -
-                """,
-                # Head, torso, and both arms
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|/
-                   |      |
-                   |      
-                   -
-                """,
-                # Head, torso, and one arm
-                """
-                   --------
-                   |      |
-                   |      O
-                   |     \\|
-                   |      |
-                   |     
-                   -
-                """,
-                # Head and torso
-                """
-                   --------
-                   |      |
-                   |      O
-                   |      |
-                   |      |
-                   |     
-                   -
-                """,
-                # Head
-                """
-                   --------
-                   |      |
-                   |      O
-                   |    
-                   |      
-                   |     
-                   -
-                """,
-                # Initial empty state
-                """
-                   --------
-                   |      |
-                   |      
-                   |    
-                   |      
-                   |     
-                   -
-                """
+def display_hangman(wrong_guesses):
+    stages = [
+        """
+           --------
+           |      |
+           |      
+           |    
+           |      
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |    
+           |      
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |      |
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|
+           |      |
+           |     
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |      
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / 
+           -
+        """,
+        """
+           --------
+           |      |
+           |      O
+           |     \\|/
+           |      |
+           |     / \\
+           -
+        """
     ]
-    return stages[attempts]
+    return stages[wrong_guesses]
 
 def hangman():
-    word = choose_word()
+    print("Player 1: Enter a word for Player 2 to guess.")
+    word = getpass.getpass("Enter word: ").lower()
+
     guessed_letters = set()
-    attempts = 6  # Number of allowed wrong guesses
+    wrong_guesses = 0
+    max_wrong_guesses = 6
 
-    print("Welcome to Hangman!")
+    print("\n" * 50)
+    print("Welcome to Hangman! Player 2, start guessing.")
 
-    while attempts > 0:
-        print(display_hangman(attempts))
+    while wrong_guesses < max_wrong_guesses:
+        print(display_hangman(wrong_guesses))
         print("\nGuessed letters:", " ".join(sorted(guessed_letters)))
         print("\nWord:", display_word(word, guessed_letters))
         
@@ -101,17 +95,40 @@ def hangman():
 
         guessed_letters.add(guess)
 
-        if guess in word:
-            print("Good guess!")
+        if guess not in word:
+            wrong_guesses += 1
+            print(f"Wrong guess! You have {max_wrong_guesses - wrong_guesses} attempts left.")
         else:
-            attempts -= 1
-            print(f"Wrong guess! You have {attempts} attempts left.")
-        
+            print("Good guess!")
+
         if all(letter in guessed_letters for letter in word):
-            print("\nCongratulations! You've guessed the word:", word)
+            print("\nCongratulations, Player 2! You've guessed the word:", word)
             return
 
-    print(display_hangman(attempts))
-    print("\nSorry, you've run out of guesses. The word was:", word)
+    print(display_hangman(wrong_guesses))
+    print("\nSorry, Player 2. You've run out of guesses. The word was:", word)
 
-hangman()
+def display_menu():
+    print("1. Play Game")
+    print("2. Exit Game")
+    print("3. Restart Game")
+    return input("Enter your choice (1-3): ")
+
+def main():
+    while True:
+        choice = display_menu()
+
+        if choice == "1":
+            hangman()
+        elif choice == "2":
+            print("Exiting game. Goodbye!")
+            break
+        elif choice == "3":
+            print("Restarting game...")
+            continue
+        else:
+            print("Invalid choice. Please enter 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
+
